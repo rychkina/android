7dac6b5bbbf2d999ccf24cd6b141dd4cbe189c4b
rycyulmx <jry@ciklum.com>
2013-04-01 17:33:31 +0300

    Add tests for MainWrapperActivity

--- WigandTest/src/mobi/mobiletech/apps/android/wigand/tests/MainWrapperActivityTest.java
+++ WigandTest/src/mobi/mobiletech/apps/android/wigand/tests/MainWrapperActivityTest.java
@@ -1,29 +1,64 @@
 package mobi.mobiletech.apps.android.wigand.tests;
 
+import android.content.Intent;
 import android.test.ActivityUnitTestCase;
 import android.test.suitebuilder.annotation.SmallTest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import mobi.mobiletech.apps.android.wigand.MainWrapperActivity;
 
-/**
- * This is a simple framework for a test of an Application.  See
- * {@link android.test.ApplicationTestCase ApplicationTestCase} for more information on
- * how to write and extend Application tests.
- * <p/>
- * To run this test, you can type:
- * adb shell am instrument -w \
- * -e class mobi.mobiletech.apps.android.wigand.tests.MainWrapperActivityTest \
- * com.example.Android.tests/android.test.InstrumentationTestRunner
- */
 public class MainWrapperActivityTest extends ActivityUnitTestCase<MainWrapperActivity> {
+    MainWrapperActivity mainWrapperActivity;
+    private Intent mStartIntent;
+    public static final String TEST_WIGAND_URL = "http://wigand.wew.io/";
+    public static final String TEST_WIGAND_SERVICE_URL = "http://wigand.wew.io/services.html";
 
     public MainWrapperActivityTest() {
         super(MainWrapperActivity.class);
     }
 
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        mStartIntent = new Intent(Intent.ACTION_MAIN);
+        this.startActivity(mStartIntent, null, null);
+        mainWrapperActivity = getActivity();
+    }
+
     @SmallTest
-    public void testMain() {
-        MainWrapperActivity mainWrapperActivity = new MainWrapperActivity();
+    public void testActivityLoading() {
         assertNotNull(mainWrapperActivity);
+    }
+
+    @SmallTest
+    public void testWebView() {
+
+        WebView webView = mainWrapperActivity.getmWebView();
+        assertNotNull(webView);
+        assertTrue(webView.isHorizontalScrollBarEnabled());
+        assertTrue(webView.isVerticalScrollBarEnabled());
+        assertNotNull(webView.getSettings());
+        WebSettings testSettings = webView.getSettings();
+        assertTrue(testSettings.getJavaScriptEnabled());
+        //assertEquals(TEST_WIGAND_URL, webView.getUrl());
+
+    }
+
+    @SmallTest
+    public void testMenu() {
+       try {
+            mainWrapperActivity.onCreateOptionsMenu(null);
+            fail("Error in menu initialization");
+        } catch (Exception e) {
+            assertEquals(NullPointerException.class, e.getClass());
+        }
+
+    }
+
+    @SmallTest
+    public void testUrls() {
+        assertEquals(TEST_WIGAND_URL, MainWrapperActivity.WIGAND_URL);
+        assertEquals(TEST_WIGAND_SERVICE_URL, MainWrapperActivity.WIGAND_SERVICES_HTML);
     }
 
 }
